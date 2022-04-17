396b66ba34d974172341a71f4989bc3bb6b85bcf
tholub99 <tholub99@gmail.com>
2022-04-17 19:05:59 -0400

    Create VirtualGamepad and Simulate 2nd Player

--- src/gamepad.py
+++ src/gamepad.py
@@ -1,51 +1,111 @@
 import vgamepad as vg
 
 XboxKeyCodeMap = {
-    "BTN_DPUP": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP,
-    "BTN_DPDOWN": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_DOWN,
-    "BTN_DPLEFT": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT,
-    "BTN_DPRIGHT": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_RIGHT,
-    "BTN_SELECT": vg.XUSB_BUTTON.XUSB_GAMEPAD_START,
-    "BTN_START": vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK,
-    "BTN_THUMBL": vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB,
-    "BTN_THUMBR": vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_THUMB,
-    "BTN_TL": vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER,
-    "BTN_TR": vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER,
-    "BTN_SOUTH": vg.XUSB_BUTTON.XUSB_GAMEPAD_A,
-    "BTN_EAST": vg.XUSB_BUTTON.XUSB_GAMEPAD_B,
-    "BTN_WEST": vg.XUSB_BUTTON.XUSB_GAMEPAD_X,
-    "BTN_NORTH": vg.XUSB_BUTTON.XUSB_GAMEPAD_Y
+    'BTN_DPUP': vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP,
+    'BTN_DPDOWN': vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_DOWN,
+    'BTN_DPLEFT': vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT,
+    'BTN_DPRIGHT': vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_RIGHT,
+    'BTN_SELECT': vg.XUSB_BUTTON.XUSB_GAMEPAD_START,
+    'BTN_START': vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK,
+    'BTN_THUMBL': vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB,
+    'BTN_THUMBR': vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_THUMB,
+    'BTN_TL': vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER,
+    'BTN_TR': vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER,
+    'BTN_SOUTH': vg.XUSB_BUTTON.XUSB_GAMEPAD_A,
+    'BTN_EAST': vg.XUSB_BUTTON.XUSB_GAMEPAD_B,
+    'BTN_WEST': vg.XUSB_BUTTON.XUSB_GAMEPAD_X,
+    'BTN_NORTH': vg.XUSB_BUTTON.XUSB_GAMEPAD_Y
+}
+
+XboxAbsCodeMap = {
+    'ABS_Z': 'L_TRIGGER',
+    'ABS_RZ': 'R_TRIGGER',
+    'ABS_HAT0X': 'DPAD_HORIZONTAL',
+    'ABS_HAT0Y': 'DPAD_VERTICAL',
+    'ABS_Y': 'L_JOYSTICK_VERTICAL',
+    'ABS_X': 'L_JOYSTICK_HORIZONTAL',
+    'ABS_RY': 'R_JOYSTICK_VERTICAL',
+    'ABS_RX': 'R_JOYSTICK_HORIZONTAL'
 }
 
 class XboxGamepad:
     def __init__(self):
         self.gamepad = vg.VX360Gamepad()
         self.gamepad.reset()
-        gamepad.update()
+        self.gamepad.update()
+        
+        self.LJ = [0,0]
+        self.RJ = [0,0]
         
     def PressKey(self, code):
-        self.gamepad.press_button(button=code)
+        self.gamepad.press_button(button=XboxKeyCodeMap[code])
         self.gamepad.update()
         
     def ReleaseKey(self, code):
-        self.gamepad.release_button(button=code)
+        self.gamepad.release_button(button=XboxKeyCodeMap[code])
         self.gamepad.update()
+    
+    def SetAbsInput(self, code, state):
+        code = XboxAbsCodeMap[code]
+        if(code == 'DPAD_VERTICAL'):
+            self.SetDPadY(state)
+        elif(code == 'DPAD_HORIZONTAL'):
+            self.SetDPadX(state)
+        elif(code == 'L_TRIGGER'):
+            self.SetLeftTrigger(state)
+        elif(code == 'R_TRIGGER'):
+            self.SetRightTrigger(state)
+        elif(code == 'L_JOYSTICK_VERTICAL'):
+            self.SetLeftJoystickY(state)
+        elif(code == 'L_JOYSTICK_HORIZONTAL'):
+            self.SetLeftJoystickX(state)
+        elif(code == 'R_JOYSTICK_VERTICAL'):
+            self.SetRightJoystickY(state)
+        elif(code == 'R_JOYSTICK_HORIZONTAL'):
+            self.SetRightJoystickX(state)
+            
+        self.gamepad.update()
+    
+    def SetDPadX(self, val):
+        if(val == 0):
+            self.gamepad.release_button(button=XboxKeyCodeMap['BTN_DPLEFT'])
+            self.gamepad.release_button(button=XboxKeyCodeMap['BTN_DPRIGHT'])
+        elif(val == 1):
+            self.gamepad.press_button(button=XboxKeyCodeMap['BTN_DPRIGHT'])
+        else:
+            self.gamepad.press_button(button=XboxKeyCodeMap['BTN_DPLEFT'])
+    
+    def SetDPadY(self, val):
+        if(val == 0):
+            self.gamepad.release_button(button=XboxKeyCodeMap['BTN_DPDOWN'])
+            self.gamepad.release_button(button=XboxKeyCodeMap['BTN_DPUP'])
+        elif(val == 1):
+            self.gamepad.press_button(button=XboxKeyCodeMap['BTN_DPDOWN'])
+        else:
+            self.gamepad.press_button(button=XboxKeyCodeMap['BTN_DPUP'])
         
-    def MoveLeftTrigger(self, val):
+            
+    def SetLeftTrigger(self, val):
         self.gamepad.left_trigger(value=val)
-        self.gamepad.update()
         
-    def MoveRightTrigger(self, val):
+    def SetRightTrigger(self, val):
         self.gamepad.right_trigger(value=val)
-        self.gamepad.update()
         
-    def MoveLeftJoystick(self, x, y):
-        self.gamepad.left_joystick(x_value=x, y_value=y)
-        self.gamepad.update()
+    def SetLeftJoystickX(self, x):
+        self.gamepad.left_joystick(x_value=x, y_value=self.LJ[1])
+        self.LJ[0] = x
         
-    def MoveRightJoystick(self, x, y):
-        self.gamepad.right_joystick(x_value=x, y_value=y)
-        self.gamepad.update()
+    def SetLeftJoystickY(self, y):
+        self.gamepad.left_joystick(x_value=self.LJ[0], y_value=y)
+        self.LJ[1] = y
+        
+    def MoveRightJoystickX(self, x):
+        self.gamepad.right_joystick(x_value=x, y_value=self.RJ[1])
+        self.RJ[0] = x
+        
+    def MoveRightJoystickY(self, y):
+        self.gamepad.right_joystick(x_value=self.RJ[0], y_value=y)
+        self.RJ[1] = y
         
 class DualshockGamepad:
     def __init__(self):
--- src/main.py
+++ src/main.py
@@ -1,18 +1,30 @@
 from inputs import get_gamepad
 from inputs import devices
 from player import Player
+import gamepad
 
 class Host:
     def __init__(self):
         self.players = []
 
 def main():
+    vPad = gamepad.XboxGamepad()
     """Just print out some event infomation when the gamepad is used."""
     for device in devices:
         print(device)
     while 1:
         events = get_gamepad()
         for event in events:
+            if(event.ev_type == 'Key'): #Static Button
+                
+                if(event.state == 1): #keyPress
+                    vPad.PressKey(event.code)
+                elif(event.state == 0): #keyRelease
+                    vPad.ReleaseKey(event.code)
+                    
+            elif(event.ev_type == 'Absolute'): #Varying Button
+                vPad.SetAbsInput(event.code, event.state)        
+                
             print(event.ev_type, event.code, event.state)
 
 if __name__ == "__main__":
